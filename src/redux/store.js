b412d1071ffe5ce7ed157428679fbdbbec983e54
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-04-03 23:36:47 +0630

    redux setup done

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import {  applyMiddleware, createStore } from "redux";
 import rootReducer from './rootReducer';
 import logger from "redux-logger";
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-let store = createStore(rootReducer,applyMiddleware(logger));
+let store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
